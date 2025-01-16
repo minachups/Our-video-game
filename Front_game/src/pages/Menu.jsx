69a01adcdf7f51b66d4d872084f0d9c54b05881b
Hoa <h_cong@hetic.eu>
2025-01-16 17:00:07 +0100

    reogarnisation

--- Front_game/src/pages/Menu.jsx
+++ Front_game/src/pages/Menu.jsx
@@ -1,37 +1,10 @@
 import React, { useState } from 'react';
-import '../styles/Menu.css';
-import wallpaper_menu from '../assets/images/wallpaper_menu.png';
-
+import Menu_1 from '../Menu_principal/Menu_principal.jsx';
 
 function Menu() {
-  const [musicOn, setMusicOn] = useState(true);
-
-  const handleMusicToggle = () => {
-    setMusicOn(!musicOn);
-  };
 
   return (
-    <div className="menu-container">
-      <img src={wallpaper_menu}/>
-      <div className="menu-overlay">
-        <h1>MEMORY GAME</h1>
-        <div className = "buttons">
-          <button>START</button>
-          <button>SHOP</button>
-        </div>
-        <div className="music-toggle">
-          <label htmlFor="music-checkbox">Music On</label>
-          <input
-            type="checkbox"
-            id="music-checkbox"
-            checked={musicOn}
-            onChange={handleMusicToggle}
-          />
-        </div>
-      </div>
-    </div>
-    
+    <Menu_1/>
   );
 }
-
 export default Menu;
