186c8d4563a0c32edc346912ae3b845f3c098df3
Quentin-LTN <loic.quentin.n.t8@gmail.com>
2025-01-21 14:03:21 +0100

    init

---
+++ Front_game/src/Test.jsx
@@ -0,0 +1,21 @@
+// import 
+
+function Test(){
+   /* const content[
+        {
+            name:"foufou",
+            type:"rock",
+        }
+    ]*/
+    return(
+        <div>
+            <h1>la saucisse</h1>
+            <h2>La crevette</h2>
+            
+        </div>
+
+        
+    )
+}
+
+export default Test;
--- Front_game/src/pages/Menu.jsx
+++ Front_game/src/pages/Menu.jsx
@@ -15,19 +15,22 @@ function Menu() {
       <img src={wallpaper_menu}/>
       <div className="menu-overlay">
         <h1>MEMORY GAME</h1>
-        <div className = "buttons">
-          <button>START</button>
-          <button>SHOP</button>
+        <div>
+            <div className = "buttons">
+            <button>START</button>
+            <button>SHOP</button>
+            </div>
+            <div className="music-toggle">
+            <label htmlFor="music-checkbox">Music On</label>
+            <input
+                type="checkbox"
+                id="music-checkbox"
+                checked={musicOn}
+                onChange={handleMusicToggle}
+            />
+            </div>
         </div>
-        <div className="music-toggle">
-          <label htmlFor="music-checkbox">Music On</label>
-          <input
-            type="checkbox"
-            id="music-checkbox"
-            checked={musicOn}
-            onChange={handleMusicToggle}
-          />
-        </div>
+        
       </div>
     </div>
     
